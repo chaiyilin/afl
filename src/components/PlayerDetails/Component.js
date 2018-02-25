d338489056516a3538598f0b2e28a44c889d2103
yilin chai <chaiyilin@hotmail.com>
2018-02-25 22:06:50 +1100

    style polish

--- src/components/PlayerDetails/Component.js
+++ src/components/PlayerDetails/Component.js
@@ -12,20 +12,22 @@ export const Component = ({ player, playerAverage, playerTotal, playerBestOfSeas
 				<Player {...player}></Player>
 			</div>
 
-			<div className={classnames('card', { card_secondary: deviceSizeType === LARGE })}>
+			<div className={classnames('card', 'card_subsequetial', { card_secondary: deviceSizeType === LARGE })}>
 				<Indicators {...playerTotal} isShowingRed={false} title="TOTAL" deviceSizeType={deviceSizeType}></Indicators>
 			</div>
 
 			{(deviceSizeType === SMALL || deviceSizeType === MEDIUM) &&
-				(<div className={classnames('card', { card_secondary: deviceSizeType === LARGE })}>
+				(<div className={classnames('card', 'card_subsequetial', { card_secondary: deviceSizeType === LARGE })}>
 					<Indicators {...playerBestOfSeason} deviceSizeType={deviceSizeType} isShowingRed={true} title="BEST OF SEASON"></Indicators>
 				</div>
 				)
 			}
-			<div className={classnames('card', { card_secondary: deviceSizeType === LARGE })}>
-				{deviceSizeType === LARGE &&
-					<Indicators {...playerAverage} deviceSizeType={deviceSizeType} isShowingRed={true} title="AVERAGE"></Indicators>}
-			</div>
+
+			{deviceSizeType === LARGE &&
+				<div className={classnames('card', 'card_subsequetial', { card_secondary: deviceSizeType === LARGE })}>
+					<Indicators {...playerAverage} deviceSizeType={deviceSizeType} isShowingRed={true} title="AVERAGE"></Indicators>
+				</div>
+			}
 		</div>
 
 	)
